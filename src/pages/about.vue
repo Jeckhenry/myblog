e56c409908e139363d4572163bfc409b5280a279
Jeckhenry <571977485@qq.com>
2018-09-27 15:52:54 +0800

    lxx 文档

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,16 +1,35 @@
 <template>
     <div>
-      关于
+      <div class="labelhead">关于我</div>
+      <div class="myself">自我介绍</div>
+      <div class="myintroduce">
+          关于自己，好像没什么可说的，以后有了再说吧！
+      </div>
+      <footerEl></footerEl>
     </div>
 </template>
 
 <script>
   //关于
+    import footerEl from '../components/footerEl'
     export default {
-        name: "about"
+        name: "about",
+      components: {footerEl}
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .labelhead {
+    font-size: 2rem;
+    color: #666;
+    padding: 1rem 0;
+  }
+  .myself{
+    font-size: 1.8rem;
+    padding: .5rem 0;
+    border-bottom: 2px solid #409EFF;
+  }
+  .myintroduce{
+    padding: .7rem;
+  }
 </style>
--- src/pages/lookup.vue
+++ src/pages/lookup.vue
@@ -1,35 +1,72 @@
 <template>
-    <div>
-      <div class="labelhead">站内搜索</div>
-      <div class="lookup">
-        <input type="text">
-        <button>搜索</button>
-      </div>
-      <footerEl></footerEl>
+  <div>
+    <div class="labelhead">站内搜索</div>
+    <div class="lookup">
+      <input type="text" v-model="keyval">
+      <i @click="clear" v-show="showClear">X</i>
+      <button>搜索</button>
     </div>
+    <div class="article">
+
+    </div>
+    <footerEl></footerEl>
+  </div>
 </template>
 
 <script>
   //查找
   import footerEl from '../components/footerEl'
-    export default {
-        name: "lookup",
-        components: { footerEl }
+
+  export default {
+    name: "lookup",
+    data() {
+      return {
+        keyval: '',
+        showClear: false
+      }
+    },
+    watch: {
+      keyval:function(newval){
+        if(!newval){
+          this.showClear = false;
+          return false;
+        }
+        this.showClear = true;
+      }
+    },
+    components: {footerEl},
+    methods:{
+      clear(){
+        this.keyval = '';
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
-  .labelhead{
+  .labelhead {
     font-size: 2rem;
     color: #666;
     padding: 1rem 0;
   }
-  .lookup{
+  .article{
+    min-height: 200px;
+  }
+  .lookup {
     font-size: 1.6rem;
     padding: .2rem 0;
     width: 100%;
     position: relative;
-    input{
+
+    i{
+      font-style: normal;
+      position: absolute;
+      top: 10px;
+      left: calc(100% - 130px);
+      z-index: 3;
+      color: #409EFF;
+    }
+    input {
       width: 100%;
       box-sizing: border-box;
       height: 40px;
@@ -40,7 +77,7 @@
       padding: 0 100px 0 10px;
       outline: none;
     }
-    button{
+    button {
       height: 40px;
       width: 100px;
       position: absolute;
@@ -54,7 +91,7 @@
       top: 3px;
       left: calc(100% - 99px);
       outline: none;
-      &:active{
+      &:active {
         opacity: .5;
       }
     }
